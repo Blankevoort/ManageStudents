2daccd45fdcad8b521da3fcc071a670f0ca0fa91
Moeen Sedaqati <moeensedaghaty86@gmail.com>
2023-03-05 15:54:02 +0330

    Some Of The Main Edits

---
+++ manageStudents/src/layouts/MainLayout.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="/MainImages/Logo.png" />
+          </q-avatar>
+          شهیدان منا
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="rightDrawerOpen" side="right" behavior="mobile">
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer class="bg-transparent">
+      <q-img src="/MainImages/Footer.png" />
+    </q-footer>
+  </q-layout>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const rightDrawerOpen = ref(false);
+
+    return {
+      rightDrawerOpen,
+      toggleRightDrawer() {
+        rightDrawerOpen.value = !rightDrawerOpen.value;
+      },
+    };
+  },
+};
+</script>
