1ff85d53684c58b39d85157b4391000b814cda2c
Moeen Sedaqati <moeensedaghaty86@gmail.com>
2023-04-19 01:27:31 +0330

    Darkmode Added With Some Changes

--- manageStudents/src/layouts/MainLayout.vue
+++ manageStudents/src/layouts/MainLayout.vue
@@ -1,6 +1,8 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <q-header class="bg-primary">
+    <q-header
+      class="bg-primary flex justify-between content-center align-center"
+    >
       <q-toolbar>
         <q-toolbar-title>
           <q-avatar>
@@ -8,6 +10,14 @@
           </q-avatar>
           <span class="q-px-sm">شهیدان منا</span>
         </q-toolbar-title>
+
+        <q-toggle
+          v-model="darkMode"
+          checked-icon="dark_mode"
+          color="grey-9"
+          unchecked-icon="light_mode"
+          @click="toggleDarkMode"
+        />
       </q-toolbar>
     </q-header>
 
@@ -21,54 +31,84 @@
 import { ref, onBeforeMount } from "vue";
 import { api } from "src/boot/axios";
 
-import { useQuasar, Cookies } from "quasar";
+import { useQuasar, Cookies, Dark } from "quasar";
+import { watch } from "vue";
 
 export default {
   setup() {
     // Taarife zarf ha
 
     const $q = useQuasar();
-    const toggleLeftDrawer = ref(false);
+    const darkMode = ref(false);
+    const dark = ref(localStorage.getItem("dark"));
+    // const toggleLeftDrawer = ref(false);
 
-    const user = ref([]);
-    const loggedin = ref(false);
+    // const user = ref([]);
+    // const loggedin = ref(false);
 
     // Gereftane User as backend
 
-    function getUser() {
-      api
-        .get("auth/users/me/", {
-          headers: {
-            Authorization: "Token " + $q.cookies.get("token"),
-          },
-        })
-        .then((r) => {
-          user.value = r.data;
-          loggedin.value = true;
-        });
+    // function getUser() {
+    //   api
+    //     .get("auth/users/me/", {
+    //       headers: {
+    //         Authorization: "Token " + $q.cookies.get("token"),
+    //       },
+    //     })
+    //     .then((r) => {
+    //       user.value = r.data;
+    //       loggedin.value = true;
+    //     });
+    // }
+
+    // function ha
+
+    function toggleDarkMode() {
+      if ($q.dark.isActive) {
+        $q.dark.set(false);
+        localStorage.removeItem("dark");
+      } else {
+        $q.dark.set(true);
+        localStorage.setItem("dark", $q.dark.mode);
+      }
+    }
+
+    function checkDark() {
+      if (dark.value) {
+        $q.dark.set(true);
+        darkMode.value = true
+      } else {
+        $q.dark.set(false);
+        darkMode.value = false
+      }
     }
 
     // Function logout user
 
-    function logout() {
-      api
-        .post("auth/token/logout/")
-        .then($q.cookies.remove("token"))
-        .then(location.reload());
-    }
+    // function logout() {
+    //   api
+    //     .post("auth/token/logout/")
+    //     .then($q.cookies.remove("token"))
+    //     .then(location.reload());
+    // }
 
     onBeforeMount(() => {
-      getUser();
+      // getUser();
+      checkDark()
     });
 
     return {
-      user,
-      loggedin,
-      logout,
-      toggleLeftDrawer,
-      ToggleLeftDrawer() {
-        toggleLeftDrawer.value = !toggleLeftDrawer.value;
-      },
+      // user,
+      // loggedin,
+      // logout,
+      // toggleLeftDrawer,
+      toggleDarkMode,
+      darkMode,
+      dark,
+      checkDark,
+      // ToggleLeftDrawer() {
+      //   toggleLeftDrawer.value = !toggleLeftDrawer.value;
+      // },
     };
   },
 };
--- manageStudents/src/pages/LoginPage.vue
+++ manageStudents/src/pages/LoginPage.vue
@@ -1,13 +1,16 @@
 <template>
   <q-page class="fit row justify-around items-center content-center">
-    <div class="col-6 flex justify-center">
+    <div
+      class="q-my-md col-sm-12 col-md-6 col-lg-6 col-xl-6 flex justify-center content-center align-center"
+    >
       <q-form>
         <div class="text-h4 text-center">ورود کارکنان</div>
-        <div class="q-mt-xl">
+        <div class="q-mt-xl full-width">
           <div class="q-my-md" style="font-size: 18px">نام کاربری</div>
           <q-input
             class="text-wieght-bold"
             filled
+            v-model="username"
             label="نام کاربری خود را وارد کنید"
             style="width: 350px; height: 68px"
           >
@@ -15,11 +18,11 @@
           ></q-input>
         </div>
 
-        <div class="q-mt-md">
+        <div class="q-mt-md full-width">
           <div class="q-my-md" style="font-size: 18px">رمز ورود</div>
           <q-input
             filled
-            type="number"
+            v-model="password"
             label="رمز عبور خود را وارد کنید"
             style="width: 350px; height: 68px"
           >
@@ -37,17 +40,19 @@
         </div>
       </q-form>
     </div>
-    <div class="gt-md col-6 flex justify-center">
+    <div
+      class="q-my-md col-sm-12 col-md-6 col-lg-6 col-xl-6 flex justify-center content-center align-center gt-md"
+    >
       <div>
         <div>
           <q-img
-          class="q-ml-xl"
+            class="q-ml-xl"
             src="/MainImages/School.png"
             style="width: 335px; height: 225px"
           />
+          <div class="text-h4 text-center q-ml-md">نرم افزار مدیریت مدرسه</div>
         </div>
-        <div class="text-h4 text-center">نرم افزار مدیریت مدرسه</div>
-        <q-list
+        <q-list class="q-ml-md"
           ><q-item clickable v-ripple>
             <q-item-section avatar>
               <q-avatar
@@ -98,13 +103,195 @@
               />
             </q-item-section>
 
+            <q-item-section>دسترسی مدیر به اطلاعات دانش آموزان</q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+    </div>
+    <div
+      class="q-my-md col-sm-12 flex justify-center content-center align-center lt-md"
+    >
+      <div>
+        <div>
+          <q-img
+            class="q-ml-md"
+            src="/MainImages/School.png"
+            style="width: 335px; height: 225px"
+          />
+          <div class="text-h4 text-center">نرم افزار مدیریت مدرسه</div>
+        </div>
+        <q-list class="q-ml-md"
+          ><q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-avatar
+                color="positive"
+                text-color="white"
+                font-size="25px"
+                icon="done"
+              />
+            </q-item-section>
+
             <q-item-section
-              >دسترسی به اطلاعات دانش آموزان ، کارکنان ، معلمان و ... توسط مدیر
-              مدرسه</q-item-section
-            >
+              >مدیریت دانش آموزان با قابلیت افزودن ، حذف کردن و ویرایش
+              کردن</q-item-section
+            > </q-item
+          ><q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-avatar
+                color="positive"
+                text-color="white"
+                font-size="25px"
+                icon="done"
+              />
+            </q-item-section>
+
+            <q-item-section
+              >دسترسی های مختلف مدیر ، معلم و معاونین</q-item-section
+            > </q-item
+          ><q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-avatar
+                color="positive"
+                text-color="white"
+                font-size="25px"
+                icon="done"
+              />
+            </q-item-section>
+
+            <q-item-section
+              >قابلیت تنظیم برنامه های هفتگی کلاس ها</q-item-section
+            > </q-item
+          ><q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-avatar
+                color="positive"
+                text-color="white"
+                font-size="25px"
+                icon="done"
+              />
+            </q-item-section>
+
+            <q-item-section>دسترسی مدیر به اطلاعات دانش آموزان</q-item-section>
           </q-item>
         </q-list>
       </div>
     </div>
   </q-page>
 </template>
+
+<script>
+import { ref, onBeforeMount, computed } from "vue";
+import { api } from "src/boot/axios";
+
+import { useRouter } from "vue-router";
+import { useQuasar, Cookies } from "quasar";
+
+import { currentClassID } from "src/stores/classID";
+import { storeToRefs } from "pinia";
+
+export default {
+  setup() {
+    const $q = useQuasar();
+    const Classes = ref();
+    const user = ref([]);
+
+    const store = currentClassID();
+    const current = computed(() => store.current);
+    const setCurrent = (data) => store.setCurrent(data);
+    const showCurrent = computed(() => store.showCurrent);
+    const storeClassID = currentClassID();
+
+    const username = ref();
+    const password = ref();
+    const addClass = ref(false);
+    const classId = ref();
+
+    function AddNewClass() {
+      var id = {
+        class_id: classId.value,
+      };
+      api
+        .post("classes/", id, {
+          headers: {
+            Authorization: "Token " + $q.cookies.get("token"),
+          },
+        })
+        .then((r) => {
+          addClass.value = false;
+          location.reload();
+        });
+    }
+
+    function RemoveClass(index) {
+      api
+        .delete("classes/" + Classes.value[index].class_id + "/", {
+          headers: {
+            Authorization: "Token " + $q.cookies.get("token"),
+          },
+        })
+        .then((r) => {
+          location.reload();
+        });
+    }
+
+    function Login() {
+      api
+        .post("auth/token/login/", {
+          username: username.value,
+          password: password.value,
+        })
+        .then((r) => {
+          if (r.data.auth_token) {
+            $q.cookies.set("token", r.data.auth_token);
+            location.reload();
+            $router.push("/panel")
+          }
+        });
+    }
+
+    function getUser() {
+      api
+        .get("auth/users/me/", {
+          headers: {
+            Authorization: "Token " + $q.cookies.get("token"),
+          },
+        })
+        .then((r) => {
+          user.value = r.data;
+        });
+    }
+
+    function getClasses() {
+      api
+        .get("classes/", {
+          headers: {
+            Authorization: "Token " + $q.cookies.get("token"),
+          },
+        })
+        .then((r) => {
+          Classes.value = r.data;
+        });
+    }
+
+    onBeforeMount(() => {
+      getUser();
+      getClasses();
+    });
+
+    return {
+      classId,
+      username,
+      user,
+      password,
+      confirm,
+      Classes,
+      getUser,
+      addClass,
+      AddNewClass,
+      RemoveClass,
+      Login,
+      setCurrent,
+    };
+  },
+};
+</script>
