51416356f99634bfba2ead56a7b2527d7601335f
Moeen Sedaqati <moeensedaghaty86@gmail.com>
2023-04-18 21:44:19 +0330

    Login Page Redesigned And Font Changed

--- manageStudents/src/layouts/MainLayout.vue
+++ manageStudents/src/layouts/MainLayout.vue
@@ -1,108 +1,19 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <q-header class="bg-primary text-white">
-      <!-- Navbar -->
-
-      <div class="row justify-between">
-        <q-toolbar>
-          <q-btn
-            class="col-1"
-            dense
-            flat
-            round
-            icon="menu"
-            @click="ToggleLeftDrawer"
-          />
-
-          <q-toolbar-title class="col-6 offset-5">
-            <span class="q-px-sm">شهیدان منا</span>
-            <q-avatar>
-              <img src="/MainImages/logo.png" />
-            </q-avatar>
-          </q-toolbar-title>
-        </q-toolbar>
-      </div>
+    <q-header class="bg-primary">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="/MainImages/logo.png" />
+          </q-avatar>
+          <span class="q-px-sm">شهیدان منا</span>
+        </q-toolbar-title>
+      </q-toolbar>
     </q-header>
-
-    <q-drawer v-model="toggleLeftDrawer" side="left" behavior="mobile">
-      <!-- drawer content -->
-
-      <q-scroll-area
-        style="
-          height: calc(100% - 150px);
-          margin-top: 150px;
-          border-right: 1px solid #ddd;
-        "
-      >
-        <q-list padding class="q-ml-md">
-          <q-item clickable>
-            <q-item-section>
-              <q-btn flat to="/" class="font-big">صفحه اصلی</q-btn>
-            </q-item-section>
-          </q-item>
-
-          <q-item clickable>
-            <q-item-section>
-              <q-btn flat to="/about" class="font-big">درباره ما</q-btn>
-            </q-item-section>
-          </q-item>
-
-          <q-item v-if="loggedin" clickable>
-            <q-item-section>
-              <q-btn flat @click="logout()" icon="logout" push />
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </q-scroll-area>
-
-      <div class="text-center">
-        <q-img
-          class="absolute-top"
-          src="https://cdn.quasar.dev/img/material.png"
-          style="height: 150px"
-        >
-          <div class="absolute-bottom bg-transparent">
-            <q-avatar size="56px" class="q-mb-sm">
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-            </q-avatar>
-            <div class="text-weight-bold">{{ user.username }}</div>
-          </div>
-        </q-img>
-      </div>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
-
-    <!-- Footer -->
-
-    <q-footer reveal class="bg-transparent">
-      <q-img src="/MainImages/Footer.png" class="position-relative footer-img">
-        <div class="bg-transparent row items-center q-mt-xl footer-margin">
-          <div class="col-6">
-            <q-list>
-              <q-item clickable>
-                <q-item-section>
-                  <q-btn flat to="/" class="font-small">صفحه اصلی</q-btn>
-                </q-item-section>
-              </q-item>
-
-              <q-item clickable>
-                <q-item-section>
-                  <q-btn flat to="/about" class="font-small">درباره ما</q-btn>
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </div>
-
-          <div class="col-6">
-            این برنامه توسط دانش آموزان شهیدان منا و تنها برای استفاده ی مدیران
-            ساخته شده است.
-          </div>
-        </div>
-      </q-img>
-    </q-footer>
   </q-layout>
 </template>
 
---
+++ manageStudents/src/pages/LoginPage.vue
@@ -0,0 +1,110 @@
+<template>
+  <q-page class="fit row justify-around items-center content-center">
+    <div class="col-6 flex justify-center">
+      <q-form>
+        <div class="text-h4 text-center">ورود کارکنان</div>
+        <div class="q-mt-xl">
+          <div class="q-my-md" style="font-size: 18px">نام کاربری</div>
+          <q-input
+            class="text-wieght-bold"
+            filled
+            label="نام کاربری خود را وارد کنید"
+            style="width: 350px; height: 68px"
+          >
+            <template v-slot:append> <q-icon name="person" /> </template
+          ></q-input>
+        </div>
+
+        <div class="q-mt-md">
+          <div class="q-my-md" style="font-size: 18px">رمز ورود</div>
+          <q-input
+            filled
+            type="number"
+            label="رمز عبور خود را وارد کنید"
+            style="width: 350px; height: 68px"
+          >
+            <template v-slot:append> <q-icon name="key" /> </template
+          ></q-input>
+        </div>
+
+        <div class="q-mt-md">
+          <q-btn
+            label="ورود به پنل"
+            type="submit"
+            color="primary"
+            style="width: 350px; height: 55px; font-size: 22px"
+          />
+        </div>
+      </q-form>
+    </div>
+    <div class="gt-md col-6 flex justify-center">
+      <div>
+        <div>
+          <q-img
+          class="q-ml-xl"
+            src="/MainImages/School.png"
+            style="width: 335px; height: 225px"
+          />
+        </div>
+        <div class="text-h4 text-center">نرم افزار مدیریت مدرسه</div>
+        <q-list
+          ><q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-avatar
+                color="positive"
+                text-color="white"
+                font-size="25px"
+                icon="done"
+              />
+            </q-item-section>
+
+            <q-item-section
+              >مدیریت دانش آموزان با قابلیت افزودن ، حذف کردن و ویرایش
+              کردن</q-item-section
+            > </q-item
+          ><q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-avatar
+                color="positive"
+                text-color="white"
+                font-size="25px"
+                icon="done"
+              />
+            </q-item-section>
+
+            <q-item-section
+              >دسترسی های مختلف مدیر ، معلم و معاونین</q-item-section
+            > </q-item
+          ><q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-avatar
+                color="positive"
+                text-color="white"
+                font-size="25px"
+                icon="done"
+              />
+            </q-item-section>
+
+            <q-item-section
+              >قابلیت تنظیم برنامه های هفتگی کلاس ها</q-item-section
+            > </q-item
+          ><q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-avatar
+                color="positive"
+                text-color="white"
+                font-size="25px"
+                icon="done"
+              />
+            </q-item-section>
+
+            <q-item-section
+              >دسترسی به اطلاعات دانش آموزان ، کارکنان ، معلمان و ... توسط مدیر
+              مدرسه</q-item-section
+            >
+          </q-item>
+        </q-list>
+      </div>
+    </div>
+  </q-page>
+</template>
