65c3e957cb3f3e3be4fd9da0a26ed6cbdad23e7d
Moeen Sedaqati <moeensedaghaty86@gmail.com>
2023-04-20 20:53:28 +0330

    some basic designs for every page

---
+++ manageStudents/src/pages/V2/ClassesPage.vue
@@ -0,0 +1,184 @@
+<template>
+  <q-page class="q-mx-xl">
+    <div>
+      <div class="text-h4 q-my-md q-mb-lg">تمامی کلاس ها</div>
+
+      <q-btn-dropdown
+        class="col-12 full-width q-my-sm"
+        align="left"
+        label="کلاس 902"
+      >
+        <q-list>
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+              </q-avatar>
+            </q-item-section>
+            <q-item-section>نام دانش آموز</q-item-section>
+            <q-item-section side>
+              <q-item-label >
+                <q-btn flat dense round icon="edit"></q-btn>
+                <q-btn flat dense round icon="delete"></q-btn>
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+
+      <q-btn-dropdown
+        class="col-12 full-width q-my-sm"
+        align="left"
+        label="کلاس 803"
+      >
+        <div>
+          <q-card flat bordered>
+            <q-markup-table>
+              <thead>
+                <tr>
+                  <th class="text-left">عکس دانش آموز</th>
+                  <th class="text-left">نام خانوادگی</th>
+                  <th class="text-right">کدملی</th>
+                  <th class="text-right">سریال شناسنام خانوادگیه</th>
+                  <th class="text-right">شماره تلفن</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td class="text-left">
+                    <q-avatar size="32px">
+                      <q-img
+                        src="https://cdn.quasar.dev/img/boy-avatar.png"
+                      ></q-img>
+                    </q-avatar>
+                  </td>
+                  <td class="text-left">عبدالرحمان</td>
+                  <td class="text-right">1112223334</td>
+                  <td class="text-right">111111-111</td>
+                  <td class="text-right">09379608155</td>
+                </tr>
+                <tr>
+                  <td class="text-left">
+                    <q-avatar size="32px">
+                      <q-img
+                        src="https://cdn.quasar.dev/img/boy-avatar.png"
+                      ></q-img>
+                    </q-avatar>
+                  </td>
+                  <td class="text-left">عبدالرحمان</td>
+                  <td class="text-right">1112223334</td>
+                  <td class="text-right">111111-111</td>
+                  <td class="text-right">09379608155</td>
+                </tr>
+                <tr>
+                  <td class="text-left">
+                    <q-avatar size="32px">
+                      <q-img
+                        src="https://cdn.quasar.dev/img/boy-avatar.png"
+                      ></q-img>
+                    </q-avatar>
+                  </td>
+                  <td class="text-left">عبدالرحمان</td>
+                  <td class="text-right">1112223334</td>
+                  <td class="text-right">111111-111</td>
+                  <td class="text-right">09379608155</td>
+                </tr>
+                <tr>
+                  <td class="text-left">
+                    <q-avatar size="32px">
+                      <q-img
+                        src="https://cdn.quasar.dev/img/boy-avatar.png"
+                      ></q-img>
+                    </q-avatar>
+                  </td>
+                  <td class="text-left">عبدالرحمان</td>
+                  <td class="text-right">1112223334</td>
+                  <td class="text-right">111111-111</td>
+                  <td class="text-right">09379608155</td>
+                </tr>
+                <tr>
+                  <td class="text-left">
+                    <q-avatar size="32px">
+                      <q-img
+                        src="https://cdn.quasar.dev/img/boy-avatar.png"
+                      ></q-img>
+                    </q-avatar>
+                  </td>
+                  <td class="text-left">عبدالرحمان</td>
+                  <td class="text-right">1112223334</td>
+                  <td class="text-right">111111-111</td>
+                  <td class="text-right">09379608155</td>
+                </tr>
+                <tr>
+                  <td class="text-left">
+                    <q-avatar size="32px">
+                      <q-img
+                        src="https://cdn.quasar.dev/img/boy-avatar.png"
+                      ></q-img>
+                    </q-avatar>
+                  </td>
+                  <td class="text-left">عبدالرحمان</td>
+                  <td class="text-right">1112223334</td>
+                  <td class="text-right">111111-111</td>
+                  <td class="text-right">09379608155</td>
+                </tr>
+                <tr>
+                  <td class="text-left">
+                    <q-avatar size="32px">
+                      <q-img
+                        src="https://cdn.quasar.dev/img/boy-avatar.png"
+                      ></q-img>
+                    </q-avatar>
+                  </td>
+                  <td class="text-left">عبدالرحمان</td>
+                  <td class="text-right">1112223334</td>
+                  <td class="text-right">111111-111</td>
+                  <td class="text-right">09379608155</td>
+                </tr>
+                <tr>
+                  <td class="text-left">
+                    <q-avatar size="32px">
+                      <q-img
+                        src="https://cdn.quasar.dev/img/boy-avatar.png"
+                      ></q-img>
+                    </q-avatar>
+                  </td>
+                  <td class="text-left">عبدالرحمان</td>
+                  <td class="text-right">1112223334</td>
+                  <td class="text-right">111111-111</td>
+                  <td class="text-right">09379608155</td>
+                </tr>
+                <tr>
+                  <td class="text-left">
+                    <q-avatar size="32px">
+                      <q-img
+                        src="https://cdn.quasar.dev/img/boy-avatar.png"
+                      ></q-img>
+                    </q-avatar>
+                  </td>
+                  <td class="text-left">عبدالرحمان</td>
+                  <td class="text-right">1112223334</td>
+                  <td class="text-right">111111-111</td>
+                  <td class="text-right">09379608155</td>
+                </tr>
+                <tr>
+                  <td class="text-left">
+                    <q-avatar size="32px">
+                      <q-img
+                        src="https://cdn.quasar.dev/img/boy-avatar.png"
+                      ></q-img>
+                    </q-avatar>
+                  </td>
+                  <td class="text-left">عبدالرحمان</td>
+                  <td class="text-right">1112223334</td>
+                  <td class="text-right">111111-111</td>
+                  <td class="text-right">09379608155</td>
+                </tr>
+              </tbody>
+            </q-markup-table>
+          </q-card>
+        </div>
+      </q-btn-dropdown>
+    </div>
+  </q-page>
+</template>
--- manageStudents/src/router/routes.js
+++ manageStudents/src/router/routes.js
@@ -23,14 +23,28 @@ const routes = [
     path: "/login",
     component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: "", component: () => import("src/pages/LoginPage.vue") },
+      { path: "", component: () => import("src/pages/V2/LoginPage.vue") },
     ],
   },
   {
     path: "/dashboard",
     component: () => import("layouts/ManagmentPanel.vue"),
     children: [
-      { path: "", component: () => import("src/pages/DashboardPage.vue") },
+      { path: "", component: () => import("src/pages/V2/DashboardPage.vue") },
+    ],
+  },
+  {
+    path: "/classes",
+    component: () => import("layouts/ManagmentPanel.vue"),
+    children: [
+      { path: "", component: () => import("src/pages/V2/ClassesPage.vue") },
+    ],
+  },
+  {
+    path: "/students",
+    component: () => import("layouts/ManagmentPanel.vue"),
+    children: [
+      { path: "", component: () => import("src/pages/V2/StudentsPage.vue") },
     ],
   },
 
